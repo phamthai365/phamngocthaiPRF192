4ea13bd087b33edaa974d31711c91b713efc8da7
phamthai <phamthai365@gmail.com>
2026-01-09 11:22:01 +0700

    songuyen

---
+++ slot2/bai 1c.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	//Khai bao 1 bien so nguyen
+	int a;
+	//Khai tao gia tri bat ky
+	a =5;
+	//In ra man hinh
+	printf("a=%d\n", a);
+	//Gán giá trị mới cho a
+	a = 10;
+	printf("a=%d",a);
+}
