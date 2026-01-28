4f2700769d849a6e0ac2b76eb341d0ce0940937a
phamthai <phamthai365@gmail.com>
2026-01-28 09:07:11 +0700

    ex

--- Workshop/Workshop1/Ex_6.c
+++ Workshop/Workshop1/Ex_6.c
@@ -1,30 +1,37 @@
-#include<stdio.h>
-#include<math.h>
- int main(){
- 	float cc, gk , ck , tong;
- 	char grade;
- 	printf("Nhap diem chuyen can:");
- 	scanf("%f", &cc);
- 	printf("Nhap diem giua ky:");
- 	scanf("%f", &gk);
- 	printf("Nhap diem cuoi ky:");
- 	scanf("%f", &ck);
- 	tong = cc*0.1 + gk*0.3 + ck*0.6;
- 	printf("Diem tong ket: %.2f\n",tong);
-// Xep loai 
-    if (tong >= 8.5) grade = 'A';
-	else
-	    if(tong >=7.0) grade = 'B';
-    else
-        if(tong >=5.5) grade = 'C';
-    else
-	    if(tong >=4.0) grade = 'D';
-	else grade = 'F';
-	printf("Diem chu: %c\n",grade);
-// Xep tot nghiep
-    if(cc >=4 && gk >=4 && ck >=4 && grade != 'F'){
-    	printf("Du dieu kien tot nghiep\n");	
-  } else {
-  	    printf("Khong du dieu kien tot nghiep\n");
-  }
+#include <stdio.h>
+int main () {
+	float chuyenCan, giuaKy, cuoiKy;
+	float tongKet;
+	char diemChu;
+	printf("Nhap diem chuyen can, giua ky, cuoi ky:");
+	int kq = scanf("%f %f %f",&chuyenCan, &giuaKy, &cuoiKy);
+	printf("kq nhap: %d",kq);
+	if(kq !=3 || chuyenCan <0 || giuaKy<0 || chuyenCan <0 ||cuoiKy<0 ||chuyenCan >10 ||giuaKy>10 ||cuoiKy>10)
+	{
+		printf("\nNhap sai diem, diem >=0!");
+		return 0;
+	}
+	tongKet = (chuyenCan * 0.1) + (giuaKy * 0.3) + (cuoiKy * 0.6);
+	
+	if(tongKet >= 8.5) {
+		diemChu = 'A';
+	} else if(tongKet >= 7) {
+		diemChu = 'B';
+	} else if(tongKet >= 5.5) {
+		diemChu = 'C';
+	} else if(tongKet >= 4) {
+		diemChu = 'D';
+	} else {
+		diemChu = 'F';
+	}
+	
+	printf("\nDiem so: %.1f \nDiem chu: %c\n", tongKet, diemChu);
+	
+	if (chuyenCan >= 4.0 && giuaKy >= 4.0 && cuoiKy >= 4.0 && diemChu != 'F') {
+        printf("DU DIEU KIEN TOT NGHIEP\n");
+    } else {
+        printf("KHONG DU DIEU KIEN TOT NGHIEP\n");
+    }
+    
+	return 0;
 }		    
